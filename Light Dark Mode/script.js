0f89214631b2219b2d0b04402b74e868ebc6a2aa
Dipa <dipamitro63@gmail.com>
2023-10-01 13:30:33 +0600

    alb

--- Light Dark Mode/script.js
+++ Light Dark Mode/script.js
@@ -56,3 +56,16 @@ if (currentTheme) {
     darkMode();
   }
 }
+// const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// function switchTheme(event){
+//     // console.log(event.target.checked)
+//     if(event.target.checked){
+//         document.documentElement.setAttribute("data-theme", 'dark')
+//     }
+//     else{
+//         document.documentElement.setAttribute('data-theme' ,'light')
+//     }
+
+// } 
+// toggleSwitch.addEventListener('change' , switchTheme) 
